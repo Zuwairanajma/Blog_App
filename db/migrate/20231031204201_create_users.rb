6d6bba509398a690b53c44ec49735346abe7a570
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-31 22:31:26 +0100

    Create models and some associations

--- db/migrate/20231031204201_create_users.rb
+++ db/migrate/20231031204201_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-t.string :name
-t.string :photo_link
-t.string :bio
-t.integer :posts_counter
+      t.string :name
+      t.string :photo_link
+      t.string :bio
+      t.integer :posts_counter
 
       t.timestamps
     end
