dad3691494f8e687abb9cc96f16d4893d248e98c
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-06 10:24:20 +0100

    Add controllers according to diagram template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
+# Rails.application.routes.draw do
+#   get "up" => "rails/health#show", as: :rails_health_check
+
+#   # Defines the root path route ("/")
+#   root "posts#index"
+# end
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  # root "articles#index"
 end
