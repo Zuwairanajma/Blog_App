a1a8463a54dd991d6432116affc3a0b5d732a5d6
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-07 22:38:15 +0100

    Add views for users posts and likes and comments

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+  @users = User.all
+  end
 
-  def show; end
+  def show
+  @user = User.find_by_id(params[:id])
+  if @user
+  @recent_posts = @user.most_recent_posts
+  else
+    flash[:alert] = 'User not found'
+    redirect_to root_path
+  end
+end
 end
