0fe1bff3de76b1dbd11dc57c33642b29e38a73b7
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-09 18:02:57 +0100

    Implement add form requirements

--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    def cureent_user
+    @current_user ||= User.first
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+    resources :likes, only: [:create]
+      resources :comments, only: [:new, :create] 
+  end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
