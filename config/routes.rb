4e58ae2c5938df8e060d34caa3fbbbc80ebbcf6c
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-10 02:12:02 +0100

    Add data to seed file and test form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  root 'users#index'
-
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
     resources :likes, only: [:create]
       resources :comments, only: [:new, :create] 
   end
-  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
 end
