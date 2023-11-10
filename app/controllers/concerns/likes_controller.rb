e2379c4ee6a6b1d99668992b614b8c3514765eb3
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-10 02:27:26 +0100

    Remove commented codes

--- app/controllers/concerns/likes_controller.rb
+++ app/controllers/concerns/likes_controller.rb
@@ -8,18 +8,4 @@ class LikesController < ApplicationController
     redirect_to user_post_path(@user.id, @post)
   end
 end
-# LikesController
-# class LikesController < ApplicationController
-#   def create
-#     @post = Post.find(params[:post_id])
-#     @like = current_user.likes.new(post: @post)
 
-#     if @like.save
-#       flash[:notice] = 'Post liked!'
-#     else
-#       flash[:alert] = 'Unable to like post!'
-#     end
-
-#     redirect_to user_post_path(current_user.id, @post)
-#   end
-# end
