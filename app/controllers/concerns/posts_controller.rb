c77c72e76299c76d08a90bfc5c7f9b8bfdcd1647
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-14 22:03:12 +0100

    Update controllers to exclude N+1 problems

--- app/controllers/concerns/posts_controller.rb
+++ app/controllers/concerns/posts_controller.rb
@@ -1,116 +1,57 @@
-# class PostsController < ApplicationController
-#   def index
-#     @posts = Post.includes(:author).where(author_id: params[:user_id])
-#     @user = User.find(params[:user_id])
-#   end
-
-#   def show
-#     @post = Post.includes(:author).find_by(author_id: params[:user_id], id: params[:id])
-
-#     if @post
-#       @user = @post.author
-#       # @comments = @post.comments
-#       @comments = Post.find(@post.id).comments
-#     else
-#       flash[:alert] = 'Post not found'
-#       redirect_to user_posts_path(params[:user_id])
-#     end
-#   end
-
-#   def new
-#     @user = current_user
-#     @post = Post.new
-#   end
-
-#   def create
-#     @user = current_user
-#     @post = @user.posts.new(post_params)
-#     if @post.save
-#       redirect_to user_post_path(@user, @post)
-#     else
-#       puts @user
-#       puts @post.errors.full_messages
-#       flash.now[:errors] = 'Invalid post!'
-#       render :new
-#     end
-#   end
-
-#   def destroy
-#     @post = Post.find_by(author_id: params[:user_id], id: params[:id])
-#     @post.destroy
-
-#     if @post.destroyed?
-#       flash[:notice] = 'Post deleted!'
-#       redirect_to user_posts_path(@post.author)
-#     else
-#       flash.now[:errors] = 'Unable to delete post!'
-#       redirect_to user_post_path(@post.author, @post)
-#     end
-#   end
-
-#   private
-
-#   def post_params
-#     params.require(:post).permit(:title, :text)
-#   end
-# end
-
 class PostsController < ApplicationController
-def index
-  # No eager-loading, potential N+1 issues
-  @posts = Post.where(author_id: params[:user_id])
-  @user = User.find(params[:user_id])
-end
+  def index
+    @posts = Post.includes(:author).where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
+  end
 
-def show
-  # No eager-loading, potential N+1 issues
-  @post = Post.find_by(author_id: params[:user_id], id: params[:id])
+  def show
+    @post = Post.includes(:author).find_by(author_id: params[:user_id], id: params[:id])
 
-  if @post
-    # No eager-loading, potential N+1 issues
-    @user = @post.author
-    # No eager-loading, potential N+1 issues
-    @comments = @post.comments
-  else
-    flash[:alert] = 'Post not found'
-    redirect_to user_posts_path(params[:user_id])
+    if @post
+      @user = @post.author
+      @comments = @post.comments
+
+    else
+      flash[:alert] = 'Post not found'
+      redirect_to user_posts_path(params[:user_id])
+    end
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    else
+      puts @user
+      puts @post.errors.full_messages
+      flash.now[:errors] = 'Invalid post!'
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(author_id: params[:user_id], id: params[:id])
+    @post.destroy
+
+    if @post.destroyed?
+      flash[:notice] = 'Post deleted!'
+      redirect_to user_posts_path(@post.author)
+    else
+      flash.now[:errors] = 'Unable to delete post!'
+      redirect_to user_post_path(@post.author, @post)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
 
-def new
-  @user = current_user
-  @post = Post.new
-end
-
-def create
-  @user = current_user
-  @post = @user.posts.new(post_params)
-  if @post.save
-    redirect_to user_post_path(@user, @post)
-  else
-    puts @user
-    puts @post.errors.full_messages
-    flash.now[:errors] = 'Invalid post!'
-    render :new
-  end
-end
-
-def destroy
-  @post = Post.find_by(author_id: params[:user_id], id: params[:id])
-  @post.destroy
-
-  if @post.destroyed?
-    flash[:notice] = 'Post deleted!'
-    redirect_to user_posts_path(@post.author)
-  else
-    flash.now[:errors] = 'Unable to delete post!'
-    redirect_to user_post_path(@post.author, @post)
-  end
-end
-
-private
-
-def post_params
-  params.require(:post).permit(:title, :text)
-end
-end
