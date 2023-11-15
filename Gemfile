5f9091359bfa08ad27e2bcadfee15f98597655c3
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-15 13:56:41 +0100

    Fix linters

--- Gemfile
+++ Gemfile
@@ -55,8 +55,8 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem 'web-console'
   gem 'letter_opener'
+  gem 'web-console'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -74,4 +74,4 @@ end
 gem 'rails-controller-testing'
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
     attributes = [:name]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
-  # helper_method :current_user
-  # def current_user
-  #   @current_user ||= User.first
+    # helper_method :current_user
+    # def current_user
+    #   @current_user ||= User.first
   end
 end
