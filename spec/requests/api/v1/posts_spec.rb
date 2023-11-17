086992f25e1e8cb68363afd8bf4575ab5b63bbc3
Mustakim <m.m.kiron@gmail.com>
2023-11-17 13:26:57 +0600

    Fix linter errors

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Posts', type: :request do
-<<<<<<< HEAD
-  describe "GET /index" do
-=======
   describe 'GET /index' do
->>>>>>> 2051f8325d28ec77eb82413a00b53693986a9c1b
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
