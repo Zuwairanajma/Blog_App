b8d1b2a90a3a34d728c6ad59061eba820ed94628
Zuwaira Enehuwa Sadiq <106805225+Zuwairanajma@users.noreply.github.com>
2023-11-17 08:11:36 +0100

    Revert "Authorization rules"

--- spec/factories/add_devise_to_users.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :add_devise_to_user do
-    # add test here
-  end
-end
--- spec/factories/posts.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :post do
-    # Define your post attributes here
-    title { 'Sample Post' }
-    content { 'Lorem ipsum...' }
-    author
-  end
-end
